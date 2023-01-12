ba3f007c648a1617e4a9ea580fc5b9037eb642f7
pblpz <pablolopezasir@gmail.com>
2023-01-12 16:12:31 +0100

    another one bites the dust

--- chunkyMonkey/chunkyMonkey.ts
+++ chunkyMonkey/chunkyMonkey.ts
@@ -1,6 +1,12 @@
 export function chunkyMonkey(arr: any[], size: number): any[][] {
+    const result: any = [];
 
+    while (arr.length > 0 && size > 0) {
+        result.push(arr.splice(0, size));
+    }
+
+    return result;
 }
 
-// console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
-// console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,9 @@
 export function circleOfNumbers(n: number, firstNumber: number): number {
+    let result: number = 0;
 
+    
+
+    return result;
 }
 
 // console.log(circleOfNumbers(10, 2));
